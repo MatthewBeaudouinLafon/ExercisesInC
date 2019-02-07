889e1eb4bab2ca6c3ef24c7fde687da0ca2fb597
Matthew Beaudouin-Lafon <matthew.beaudouin-lafon@students.olin.edu>
2019-02-07 16:15:08 -0500

    added 6. of problem 1

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -17,12 +17,19 @@ int main ()
     void *p2 = malloc(128);
     char *s = "Hello, World";
 
+    void *r1 = malloc(13);
+    void *r2 = malloc(13); // diff between addresses 0x10
+
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
     printf ("p2 points to %p\n", p2);
     printf ("s points to %p\n", s);
+
+    printf ("r1 points to %p\n", r1);
+    printf ("r2 points to %p\n", r2);
+
 
     return 0;
 }
