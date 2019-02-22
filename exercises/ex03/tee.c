1f46d47ea1227392d8262e30c593c0d453bc53ef
Matthew Beaudouin-Lafon <matthew.beaudouin-lafon@students.olin.edu>
2019-02-22 18:05:48 -0500

    Implemented append and ignore functionality

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -1,36 +1,33 @@
 /* Written by Matthew Beaudouin-Lafon
+Easy test: gcc tee.c -o T && cat sample.txt | ./T target.txt
 
 Copy standard input to each FILE, and also to standard output.
 
     -a, --append
             append to the given FILEs, do not overwrite
 
-    -p     diagnose errors writing to non pipes
-
-    --output-error[=MODE]
-            set behavior on write error.  See MODE below
-
-    --help display this help and exit
-
-    --version
-            output version information and exit
+    -i, --ignore-interrupts
+            ignore interrupts
 */
 
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define MAX_INPUT 120
 
 int main(int argc, char* argv[]) {
     // Interpret flags
+    char write_mode = 'w';
+
     char ch;
     while ((ch = getopt(argc, argv, "ai")) != EOF) {
         switch(ch) {
         case 'a':
-            puts("Gonna append now ok?");
+            write_mode = 'a';
             break;
         case 'i':
-            puts("I may or may not interrupt.");
+            signal(SIGINT, SIG_IGN);
             break;
         }
     }
@@ -45,22 +42,28 @@ int main(int argc, char* argv[]) {
 
     // Open File
     FILE* output;
-    if (!(output = fopen(argv[0], "w"))) {
+    if (!(output = fopen(argv[0], &write_mode))) {
         fprintf(stderr, "Cannot open file.\n");
         return 1;
     }
 
     // Loop through input
-    char* buffer[MAX_INPUT];
+    char buffer[MAX_INPUT];
     while ((fgets(buffer, MAX_INPUT, stdin) != NULL)) {
         fputs(buffer, stdout);
         fputs(buffer, output);
     }
-    
-    // Write to file
-        // fputs("Hello file!", output);
-    // Write to stdout
 
-    // Close file
+    fputs("\n", stdout); // End cleanly
     fclose(output);
+    return 0;
 }
+
+/* Reflection
+3. The main slowdown was figuring out the proper way to read input line by line.
+Lots of people have different solutions that do slightly different things. 
+Otherwise it was a pretty smooth implementation process. Head First C definitely
+helped a lot with the flag handling.
+
+
+*/
