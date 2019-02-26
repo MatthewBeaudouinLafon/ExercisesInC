ac05b9eef0afefbd1bcdf1d36f7322c72fb72d25
Matthew Beaudouin-Lafon <matthew.beaudouin-lafon@students.olin.edu>
2019-02-26 14:47:43 -0500

    hw4 progress

---
+++ exercises/ex04/trout/trout.h
@@ -0,0 +1,48 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/un.h>
+#include <stdarg.h>
+#include <syslog.h>
+
+#define BUFSIZE 1500
+
+int max_ttl;
+
+typedef struct rec {                /* outgoing UDP data */
+  u_short seq;          /* sequence number */
+} Rec;
+
+typedef struct timeval Timeval;
+typedef struct sockaddr Sockaddr;
+
+Sockaddr *sasend;    /* socket addresses for various purposes */
+Sockaddr *sarecv;
+Sockaddr *salast;
+Sockaddr *sabind;
+
+socklen_t salen;                    /* length of a socket address */
+int datalen = sizeof (Rec);         /* length of the data in a datagram */
+/* the following are a few definitions from Stevens' unp.h */
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+void loop_ttl ();
+
+
+
+
